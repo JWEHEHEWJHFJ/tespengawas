071a2965ab40ec431d1423b78ba676ad93e29a48
JWEHEHEWJHFJ <walikusman5pinrang@gmail.com>
2026-09-02 15:48:01 +0800

    Update data.js via Apps Script

--- data.js
+++ data.js
@@ -1,10 +1,17 @@
 // File ini dikelola otomatis lewat Apps Script. Jangan edit manual di GitHub.
 const DATA = [
   {
-    "sesi": "fsdf",
-    "nip": "fsf",
     "nama": "saf",
     "id": 1,
-    "ruang": "fsdfs"
+    "sesi": "fsdf",
+    "ruang": "fsdfs",
+    "nip": "fsf"
+  },
+  {
+    "sesi": "dfdf",
+    "ruang": "fsdfs",
+    "id": 2,
+    "nama": "afa",
+    "nip": "affsd"
   }
 ];
