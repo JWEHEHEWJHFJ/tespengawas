0c2194e9053c9417baba4a6d64df5b4fc4dc9e72
JWEHEHEWJHFJ <walikusman5pinrang@gmail.com>
2026-09-02 15:47:18 +0800

    Buat data.js via Apps Script

---
+++ data.js
@@ -0,0 +1,10 @@
+// File ini dikelola otomatis lewat Apps Script. Jangan edit manual di GitHub.
+const DATA = [
+  {
+    "sesi": "fsdf",
+    "nip": "fsf",
+    "nama": "saf",
+    "id": 1,
+    "ruang": "fsdfs"
+  }
+];
